af023e04a06ecc9d246c64986cdcb4750cb84885
monish <monishanand.j@gmail.com>
2023-03-06 21:59:48 +0530

    Variable

---
+++ Variable/src/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+    public static void main(String[] args) {
+        int x=123;
+        float y = 12.4f;
+        boolean a = true;
+        char c = 'A';
+        String s = "hi";
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(s);
+    }
+}
