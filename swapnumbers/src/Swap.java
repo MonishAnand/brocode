8bf6be134534eb2fc252f443517cd975c8782d97
monish <monishanand.j@gmail.com>
2023-03-06 22:12:04 +0530

    Swapvariables

---
+++ swapnumbers/src/Swap.java
@@ -0,0 +1,12 @@
+public class Swap {
+    public static void main(String[] args) {
+        int x=1;
+        int y=2;
+        int temp;
+        temp = x;
+        x=y;
+        y=temp;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
