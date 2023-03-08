4011956d664393c216e212fc7fbbe5fdfbec179d
monish <monishanand.j@gmail.com>
2023-03-08 07:46:40 +0530

    Forloop

---
+++ 13-Forloop/src/Forloop.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+public class Forloop {
+    public static void main(String[] args) {
+        for (int i=20;i>=0;i-=3){
+            System.out.println(i);
+        }
+        System.out.println("happy birthday");
+    }
+}
