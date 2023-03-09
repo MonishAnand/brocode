c431dfb4cbfbf1d155b1c1e36ad3e79e8c6f99c1
monish <monishanand.j@gmail.com>
2023-03-09 20:22:02 +0530

    Array

---
+++ 15-Arrays/src/Array.java
@@ -0,0 +1,12 @@
+public class Array {
+    public static void main(String[] args) {
+        //int[] values ={1,2,3,4,5};
+        int[] val = new int[5];
+        val[0]=1;
+        val[1]=2;
+        val[2]=3;
+        for (int i=0;i<val.length;i++){
+            System.out.println(val[i]);
+        }
+    }
+}
