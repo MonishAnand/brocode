534daa6fd01999e40312ecddf062870b63404c4c
monish <monishanand.j@gmail.com>
2023-03-09 21:36:00 +0530

    Wrapperclass

---
+++ 18-Wrapperclass/src/Wrapper.java
@@ -0,0 +1,8 @@
+public class Wrapper {
+    public static void main(String[] args) {
+        Boolean b = true;
+        if (b.equals(true)){
+            System.out.println("now boolean got methods and this goes for every wrapper class");
+        }
+    }
+}
