1afbb6a79a381109bad86a2feb1adf62dff58b0a
monish <monishanand.j@gmail.com>
2023-03-12 13:32:59 +0530

    Final

---
+++ 25-Finalkeyword/src/Final.java
@@ -0,0 +1,9 @@
+public class Final {
+    public static void main(String[] args) {
+        final double PI=3.1415;
+        //PI=4;
+        //C:\Users\Monish\IdeaProjects\brocode\25-Finalkeyword\src\Final.java:4:9
+        //java: cannot assign a value to final variable PI
+        System.out.println(PI);
+    }
+}
