5d57b9b6937a3773e3bc1fe735f89b4c33dbe361
monish <monishanand.j@gmail.com>
2023-03-12 17:40:44 +0530

    Object

---
+++ 26-Objects/src/Bike.java
@@ -0,0 +1,10 @@
+public class Bike{
+    String model="ntorq";
+    int rate = 90000;
+    void engine(){
+        System.out.println("BS6");
+    }
+    void CC(){
+        System.out.println("125cc");
+    }
+}
---
+++ 26-Objects/src/Object.java
@@ -0,0 +1,9 @@
+public class Object {
+    public static void main(String[] args) {
+        Bike b = new Bike();
+        System.out.println(b.model);
+        System.out.println(b.rate);
+        b.engine();
+        b.CC();
+    }
+}
