e223069cfcf1d8dbc61d7cb01a4b9b7ba56ba808
monish <monishanand.j@gmail.com>
2023-03-14 21:38:41 +0530

    Statickeyword2

--- 33-Statickeyword/src/Friend.java
+++ 33-Statickeyword/src/Friend.java
@@ -4,13 +4,17 @@ public class Friend {
     Friend(String name){
         this.name=name;
         nameoffriend++;
-
     }
+    /* static can be used for methods too
+    static void displayfriends(){
+        System.out.println("you have "+nameoffriend+" friends");
+    }*/
     public static void main(String[] args) {
         Friend f1= new Friend("monish");
         Friend f2 = new Friend("yogesh");
         Friend f3 = new Friend("marshal");
         System.out.println(Friend.nameoffriend);
+        //Friend.displayfriends();
 
     }
 }
