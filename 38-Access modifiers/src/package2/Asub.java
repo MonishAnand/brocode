09be6abe3906033769609245cc01578c8078dd11
monish <monishanand.j@gmail.com>
2023-03-20 19:06:33 +0530

    access modifier

---
+++ 38-Access modifiers/src/package2/Asub.java
@@ -0,0 +1,11 @@
+package package2;
+import package1.*;
+
+class Asub extends A {
+   public static void main(String[] args) {
+        /*C c = new C();
+        System.out.println(c.a);*/
+       Asub x = new Asub();
+       System.out.println(x.pro);
+    }
+}
---
+++ 38-Access modifiers/src/package2/C.java
@@ -0,0 +1,6 @@
+package package2;
+import package1.*;
+public class C {
+    String a = "default";
+    public String b = "public";
+}
