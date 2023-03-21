e087d692abc42d3cd44a10e92d91dc8a7b4221f5
monish <monishanand.j@gmail.com>
2023-03-21 21:08:59 +0530

    Encapsulation

---
+++ 39-Encapsulation/src/Main.java
@@ -0,0 +1,31 @@
+class car{
+    private String model;
+    private String name;
+    private int year;
+    car(String model, String name,int year){
+        this.model=model;
+        this.name=name;
+        this.setYear(year);
+    }
+    public String getModel(){
+        return model;
+    }
+    public String getName(){
+        return name;
+    }
+    public int getYear(){
+        return setYear(year);
+    }
+    public int setYear(int year){
+        return year;
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        car c= new car("suv","toyoto",2021);
+        c.setYear(2023);
+        System.out.println(c.getModel());
+        System.out.println(c.getName());
+        System.out.println(c.getYear());
+    }
+}
