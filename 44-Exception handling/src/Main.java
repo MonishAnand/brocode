3a2bb6996650aad438820148acf1b4c6a11a601b
monish <monishanand.j@gmail.com>
2023-03-27 20:30:01 +0530

    Exception handling

---
+++ 44-Exception handling/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+       try{
+        Scanner c = new Scanner(System.in);
+        System.out.println("enter numbers to divide");
+        int a =c.nextInt();
+        int b =c.nextInt();
+        System.out.println(a/b);
+    }
+       catch (ArithmeticException e){
+           System.out.println("you cant divide by 0 you dumb ass");
+       }
+       catch (InputMismatchException e){
+           System.out.println("you cant use another datatype u idiot");
+       }
+       catch (Exception e){
+           System.out.println("some error occurred");
+       }
+    }
+}
